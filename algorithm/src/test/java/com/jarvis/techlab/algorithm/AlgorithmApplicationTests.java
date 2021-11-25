e7f6e0d03dec1c80203fa6c0de77669b76adc869
littleniannian <jarviscrick@outlook.com>
2021-11-25 13:26:36 +0800

    add algorithm module

---
+++ algorithm/src/test/java/com/jarvis/techlab/algorithm/AlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvis.techlab.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
